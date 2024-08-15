88512c358bd1eaecfa32ed2c7b17e0b67c8eac79
under <stevenfadublagem@gmail.com>
2024-08-15 11:22:51 -0300

    (update estudo 2.0)

--- teste.py
+++ teste.py
@@ -3,15 +3,15 @@ import flet as ft
 # porta de entrada para o aplicativo flet
 def main(page: ft.page):
     # adicionando controle nas páginas
+        # adicionando um título
+    page.title = 'Falando sobre container'
+    
     page.padding = 0
 
     # aplicando o tamanho da janela/view
     page.window_width = 400
     page.window_height = 550
-
-    page.controls.append(ft.Text('Olá'))
     page.update()
 
-
 # aplicação do próprio aplicativo flat
-ft.app(port=8550, target=main)
+ft.app(target=main) 
