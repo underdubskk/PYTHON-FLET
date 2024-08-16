d95c7707bca6cbafb55097763df03f86e9204c7b
under <stevenfadublagem@gmail.com>
2024-08-16 11:07:17 -0300

    (update estudo 2.0)

--- teste.py
+++ teste.py
@@ -13,5 +13,48 @@ def main(page: ft.page):
     page.window_height = 550
     page.update()
 
+
+    # criando um botão (container)
+    c1 = ft.Container(
+        content = ft.ElevatedButton('Botão elevado no container'),
+
+        # adicionando cores ao botão (container)
+        bgcolor = '#f12345',
+        padding = 10,
+
+        # modificando a largura
+        width = 300
+        )
+
+
+    # criando um botão (container) 2
+    c2 = ft.Container(
+        content = ft.ElevatedButton('Botão elevado no container 2'),
+
+        # adicionando cores ao botão (container)
+        bgcolor = '#f12345',
+        padding = 10,
+
+        # modificando a largura
+        width = 300
+        )
+    
+
+        # criando um botão (container) 3
+    c3 = ft.Container(
+        content = ft.ElevatedButton('Botão elevado no container 3'),
+
+        # adicionando cores ao botão (container)
+        bgcolor = '#f12345',
+        padding = 10,
+
+        # modificando a largura
+        width = 300
+        )
+
+
+    # adicionando os botões (container)
+    page.add(c1, c2, c3)
+
 # aplicação do próprio aplicativo flat
-ft.app(target=main) 
+ft.app(target = main) 
